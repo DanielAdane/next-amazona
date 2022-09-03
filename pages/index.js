13778bdbb45d4789a09d79ad5e1408f97a1f07f0
ETHIO CODE <adanetesfahun3@gmail.com>
2022-09-03 15:25:12 +0300

    create layout component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-5xl">Next Amazona</h1>
-    </div>
-  );
+  return <Layout title="Home Page">Home Page</Layout>;
 }
