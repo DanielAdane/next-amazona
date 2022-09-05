a7c9624d6d16743e41ead030059a27a8b10c1f66
ETHIO CODE <adanetesfahun3@gmail.com>
2022-09-05 14:35:13 +0300

    add login functionality with next-auth

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,16 +1,43 @@
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import Layout from "../../components/Layout";
+import { signIn, useSession } from "next-auth/react";
+import { toast } from "react-toastify";
+import { getError } from "../../utils/error";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function LoginPage() {
+  const { data: session } = useSession();
+
+  const router = useRouter();
+  const { redirect } = router.query;
+
+  useEffect(() => {
+    if (session?.user) {
+      router.push(redirect || "/");
+    }
+  }, [router, session?.user, redirect]);
+
   const {
     handleSubmit,
     register,
     formState: { errors },
   } = useForm();
 
-  const submitHandler = ({ email, password }) => {
-    console.log(email, password);
+  const submitHandler = async ({ email, password }) => {
+    try {
+      const result = await signIn("credentials", {
+        redirect: false,
+        email,
+        password,
+      });
+      if (result.error) {
+        toast.error(result.error, { type: "error" });
+      }
+    } catch (error) {
+      toast.error(getError(error), { type: "error" });
+    }
   };
 
   return (
